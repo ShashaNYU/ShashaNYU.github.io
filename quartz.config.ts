1337d2988ea41a55d215c39176cdf72c553e198f
rs8938@nyu.edu <rs8938@nyu.edu>
2025-12-09 23:54:15 -0500

    test

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Quartz 4",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
